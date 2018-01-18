a355ab517c98166b7b157badfcc2e5876eb3fe03
ray <raytorres280@gmail.com>
2018-01-17 21:21:01 -0500

    vr start

--- react-vr/v2-tree/index.vr.js
+++ react-vr/v2-tree/index.vr.js
@@ -8,36 +8,52 @@ import {
   Model,
   AmbientLight,
   PointLight,
+  NativeModules,
+  Scene,
+	Text
 } from 'react-vr';
 import Tree from './components/Tree';
+import Walk from 'react-vr-walk'
 
 export default class HelloWorld extends React.Component {
+  handleInput(e) {
+    // console.log(e.handler)
+    console.log(e.target)
+  }
   render() {
     return (
-      <View>
-        <AmbientLight intensity={0.4} />
-        <PointLight
-          style={{color: 'white', transform: [{translate: [0, 400, 700]}]}}
-        />
-        <Pano source={asset('heaven.png')} />
-        <Tree
-          style={{
-            transform: [{translate: [0, -1, -5]}],
-          }}
-        />
-        <Tree
-          style={{
-            transform: [{translate: [5, -1, -5]}],
-          }}
-        />
-        <Model
-          source={{obj: asset('plane.obj'), mtl: asset('plane.mtl')}}
-          lit
-          style={{
-            transform: [{scale: [1, 1, 1]}, {translate: [0, -1, 0]}],
-          }}
-        />
-      </View>
+			<Walk panoSource={asset('heaven.png')}
+            speed={1}>
+						<Scene
+			      onInput={(e) => this.handleInput(e)}
+			      onMove={() => console.log('detected camera move')}
+			      onEnter={() => console.log('onenter scene detected')}
+			      >
+			        <AmbientLight intensity={0.4} />
+			        <PointLight
+			          style={{color: 'white', transform: [{translate: [0, 400, 700]}]}}
+			        />
+			        {/* <Pano source={asset('heaven.png')} /> */}
+			        <Tree
+			          style={{
+			            transform: [{translate: [0, -1, -5]}],
+			          }}
+			        />
+			        <Tree
+			          style={{
+			            transform: [{translate: [5, -1, -5]}],
+			          }}
+			        />
+			        <Model
+			          source={{obj: asset('plane.obj'), mtl: asset('plane.mtl')}}
+			          lit
+			          style={{
+			            transform: [{scale: [1, 1, 1]}, {translate: [0, -1, 0]}],
+			          }}
+			        />
+			      </Scene>
+      </Walk>
+
     );
   }
 }
--- react-vr/v2-tree/vr/client.js
+++ react-vr/v2-tree/vr/client.js
@@ -1,17 +1,38 @@
-// Auto-generated content.
-// This file contains the boilerplate to set up your React app.
-// If you want to modify your application, start in "index.vr.js"
+// vr/client.js
+import {Module, VRInstance} from 'react-vr-web';
 
-// Auto-generated content.
-import {VRInstance} from 'react-vr-web';
+class TeleportModule extends Module {
+  constructor() {
+    super('TeleportModule');
+    this._camera = null;
+  }
+
+  setCamera(camera) {
+    this._camera = camera;
+  }
+
+  teleportCamera(x, y, z) {
+    if (this._camera) {
+      this._camera.position.set(x, y, z);
+      // Call this to make sure anything positioned relative to the camera is set up properly:
+      this._camera.updateMatrixWorld(true);
+    }
+  }
+}
 
 function init(bundle, parent, options) {
+  const teleportModule = new TeleportModule();
   const vr = new VRInstance(bundle, 'HelloWorld', parent, {
     // Add custom options here
+    nativeModules: [ teleportModule ],
     ...options,
   });
-  vr.render = function() {
+
+  teleportModule.setCamera(vr.player.camera);
+
+  vr.render = function(timestamp) {
     // Any custom behavior you want to perform on each frame goes here
+    
   };
   // Begin the animation loop
   vr.start();
